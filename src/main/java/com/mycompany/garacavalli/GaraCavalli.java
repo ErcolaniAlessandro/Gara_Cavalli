52abdc0fa2abb88c5a1a9e8bf4cf126af6856fad
ErcolaniAlessandro <alessandroercolani22@gmail.com>
2024-10-30 21:02:16 +0100

    Update GaraCavalli.java

--- src/main/java/com/mycompany/garacavalli/GaraCavalli.java
+++ src/main/java/com/mycompany/garacavalli/GaraCavalli.java
@@ -3,36 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class GaraCavalli {
-
-    
-    static class Cavallo extends Thread {
-        private final String nome;
-        private final int lunghezzaGara;
-        private int distanzaPercorsa = 0;
-
-        public Cavallo(String nome, int lunghezzaGara) {
-            this.nome = nome;
-            this.lunghezzaGara = lunghezzaGara;
-        }
-
-        @Override
-        public void run() {
-            while (distanzaPercorsa < lunghezzaGara) {
-                distanzaPercorsa += 10; 
-                System.out.println(nome + " ha percorso " + distanzaPercorsa + " metri");
-
-                
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.println(nome + " ha terminato la gara!");
-        }
-    }
-
-    public static void main(String[] args) {
+ public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
        
