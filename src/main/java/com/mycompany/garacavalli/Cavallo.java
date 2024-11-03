1c0e996277807abd163e007080304789f9e06e22
ErcolaniAlessandro <alessandroercolani22@gmail.com>
2024-11-03 17:43:03 +0100

    Cavallo.java

--- src/main/java/com/mycompany/garacavalli/Cavallo.java
+++ src/main/java/com/mycompany/garacavalli/Cavallo.java
@@ -1,6 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.garacavalli;
 
 class Cavallo extends Thread {
         private final String nome;
